550c6c9253896763f21c285305f6a941e207dced
SkyVerge <info@skyverge.com>
2013-04-27 01:06:42 +0000

    WooCommerce Sequential Order Numbers 1.3
    Subscriptions and Pre-Orders support.  Code cleanup/refactor.  Ownership transferred to SkyVerge

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === WooCommerce Sequential Order Numbers ===
-Contributors: FoxRunSoftware
+Contributors: SkyVerge
 Tags: woocommerce, order number
 Requires at least: 3.3
-Tested up to: 3.5
-Stable tag: 1.2.4
+Tested up to: 3.5.1
+Stable tag: 1.3
 
 This plugin extends the WooCommerce e-commerce plugin by setting sequential order numbers for new orders.
 
@@ -11,21 +11,21 @@ This plugin extends the WooCommerce e-commerce plugin by setting sequential orde
 
 This plugin extends the WooCommerce e-commerce plugin by setting sequential order numbers for new orders.  If there are existing orders at the time of installation, the sequential order numbers will start with the highest current order number.
 
-If you like this plugin, but are looking for the ability to set the starting number, or to add a custom prefix/suffix to your order numbers (ie, you'd prefer something like WT101UK, WT102UK, etc) please consider my newly released paid WooCommerce Sequential Order Numbers Pro plugin, available from the [WooThemes Store](http://www.woothemes.com/products/sequential-order-numbers-pro/)
+If you like this plugin, but are looking for the ability to set the starting number, or to add a custom prefix/suffix to your order numbers (ie, you'd prefer something like WT101UK, WT102UK, etc) please consider our premium WooCommerce Sequential Order Numbers Pro plugin, available from the [WooThemes Store](http://www.woothemes.com/products/sequential-order-numbers-pro/)
 
 This plugin requires WooCommerce 1.5.6 or greater.
 
 = Feedback =
-* I am open to your suggestions and feedback - Thank you for using or trying out one of my plugins!
-* Drop me a line at [www.foxrunsoftware.com](http://www.foxrunsoftware.com)
+* We are open to your suggestions and feedback - Thank you for using or trying out one of our plugins!
+* Drop us a line at [www.skyverge.com](http://www.skyverge.com)
 
 = More =
-* Vist the [WooCommerce Sequential Order Number plugin page](http://www.foxrunsoftware.net/articles/wordpress/woocommerce-sequential-order-numbers/) for further details and a discussion of the implementation.  [Also see my other plugins](http://www.foxrunsoftware.com) or see [my WordPress.org profile page](http://profiles.wordpress.org/foxrunsoftware/)
+* Vist the [WooCommerce Sequential Order Number plugin page](http://www.skyverge.com/blog/woocommerce-sequential-order-numbers/) for further details and a discussion of the implementation.  [Also see our other plugins](http://www.skyverge.com) or see [our WordPress.org profile page](http://profiles.wordpress.org/skyverge/)
 * Find the enhanced WooCommerce Sequential Order Numbers Pro now available from [WooThemes](http://www.woothemes.com/products/sequential-order-numbers-pro/)
 
 == Installation ==
 
-1. Upload the entire 'woocommerce-sequential-order-number' folder to the '/wp-content/plugins/' directory
+1. Upload the entire 'woocommerce-sequential-order-numbers' folder to the '/wp-content/plugins/' directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Order numbers will continue sequentially from the current highest order number, or from 1 if no orders have been placed yet
 
@@ -33,50 +33,55 @@ This plugin requires WooCommerce 1.5.6 or greater.
 
 = Can I start the order numbers at a particular number? =
 
-This free version does not have that functionality, but now you can with the paid [WooCommerce Sequential Order Numbers Pro](http://www.woothemes.com/products/sequential-order-numbers-pro/)
+This free version does not have that functionality, but now you can with the premium [WooCommerce Sequential Order Numbers Pro](http://www.woothemes.com/products/sequential-order-numbers-pro/)
 
 = Can I set an order number prefix/suffix? =
 
-This free version does not have that functionality, but now you can with the paid [WooCommerce Sequential Order Numbers Pro](http://www.woothemes.com/products/sequential-order-numbers-pro/)
+This free version does not have that functionality, but now you can with the premium [WooCommerce Sequential Order Numbers Pro](http://www.woothemes.com/products/sequential-order-numbers-pro/)
 
 == Changelog ==
 
-= 1.2.4 =
-* WordPress 3.5 compatibility fix
-* Order numbers not assigned to temporary auto-draft orders created from the admin
+= 1.3 - 2013.04.26 =
+* Feature - Improved WooCommerce Subscriptions compatibility
+* Feature - Improved WooCommerce Pre-Orders compatibility
+* General code cleanup and refactor
 
-= 1.2.3 =
-* Removed WooCommerce functions, which caused a compatibility issue with other WooCommerce plugins
+= 1.2.4 - 2012.12.14 =
+* Fix - WordPress 3.5 compatibility fix
+* Fix - Order numbers not assigned to temporary auto-draft orders created from the admin
 
-= 1.2.2 =
-* Takes advantage of new action hooks/filters available in WooCommerce 1.5.6
-* Bug fix on installation to stores with more than 10 existing orders
+= 1.2.3 - 2012.06.06 =
+* Fix - Removed WooCommerce functions, which caused a compatibility issue with other WooCommerce plugins
 
-= 1.2.1 =
-* Minor updates due to WooCommerce 1.5.5 release
+= 1.2.2 - 2012.05.25 =
+* Tweak - Takes advantage of new action hooks/filters available in WooCommerce 1.5.6
+* Fix - Bug fix on installation to stores with more than 10 existing orders
 
-= 1.2.0 =
-* Added support for the order tracking page
+= 1.2.1 - 2012.05.13 =
+* Tweak - Minor updates due to WooCommerce 1.5.5 release
 
-= 1.1.2 =
-* Minor updates due to WooCommerce 1.5.4 release
+= 1.2.0 - 2012.04.21 =
+* Feature - Added support for the order tracking page
 
-= 1.1.1 =
-* Order number in the subject line of the admin new order email is fixed
+= 1.1.2 - 2012.04.18 =
+* Tweak - Minor updates due to WooCommerce 1.5.4 release
 
-= 1.1.0 =
-* Search by order number
+= 1.1.1 - 2012.04.02 =
+* Fix - Order number in the subject line of the admin new order email is fixed
 
-= 1.0.1 =
-* small bug fix
+= 1.1.0 - 2012.04.02 =
+* Feature - Search by order number
 
-= 1.0.0 =
+= 1.0.1 - 2012.04.02 =
+* Fix - small bug fix
+
+= 1.0.0 - 2012.04.02 =
 * Initial Release
 
 == Upgrade Notice ==
 
-= 1.2.2 =
+= 1.2.2 - 2012.05.25 =
 This version requires WooCommerce 1.5.6
 
-= 1.2.1 =
+= 1.2.1 - 2012.05.13 =
 This version requires WooCommerce 1.5.5
--- woocommerce-sequential-order-numbers.php
+++ woocommerce-sequential-order-numbers.php
@@ -1,210 +1,306 @@
 <?php
-/*
-Plugin Name: WooCommerce Sequential Order Numbers
-Plugin URI: http://www.foxrunsoftware.net/articles/wordpress/woocommerce-sequential-order-numbers/
-Description: Provides sequential order numbers for WooCommerce orders
-Author: Justin Stern
-Author URI: http://www.foxrunsoftware.net
-Version: 1.2.4
+/**
+ * Plugin Name: WooCommerce Sequential Order Numbers
+ * Plugin URI: http://www.skyverge.com/blog/woocommerce-sequential-order-numbers/
+ * Description: Provides sequential order numbers for WooCommerce orders
+ * Author: SkyVerge
+ * Author URI: http://www.skyverge.com
+ * Version: 1.3
+ *
+ * Copyright: (c) 2012-2013 SkyVerge, Inc. (info@skyverge.com)
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   WC-Sequential-Order-Numbers
+ * @author    SkyVerge
+ * @category  Plugin
+ * @copyright Copyright (c) 2012-2013, SkyVerge, Inc.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
 
-	Copyright: © 2012 Justin Stern (email : justin@foxrunsoftware.net)
-	License: GNU General Public License v3.0
-	License URI: http://www.gnu.org/licenses/gpl-3.0.html
-*/
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Check if WooCommerce is active
+if ( ! WC_Seq_Order_Number::is_woocommerce_active() )
+	return;
 
 /**
- * Required functions
- **/
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	
-	if ( ! class_exists( 'WC_Seq_Order_Number' ) ) {
-	 
-		class WC_Seq_Order_Number {
-			const VERSION = "1.2.4";
-			const VERSION_OPTION_NAME = "woocommerce_seq_order_number_db_version";
-			
-			public function __construct() {
-				
-				// set the custom order number on the new order.  we hook into wp_insert_post for orders which are created
-				//  from the frontend, and we hook into woocommerce_process_shop_order_meta for admin-created orders
-				add_action( 'wp_insert_post',                      array( &$this, 'set_sequential_order_number' ), 10, 2 );
-				add_action( 'woocommerce_process_shop_order_meta', array( &$this, 'set_sequential_order_number' ), 10, 2 );
-				
-				// return our custom order number for display
-				add_filter( 'woocommerce_order_number',            array( &$this, 'get_order_number' ), 10, 2);
-				
-				// order tracking page search by order number
-				add_filter( 'woocommerce_shortcode_order_tracking_order_id', array( &$this, 'find_order_by_order_number' ) );
-				
-				if ( is_admin() ) {
-					add_filter( 'request',                              array( &$this, 'woocommerce_custom_shop_order_orderby' ), 20 );
-					add_filter( 'woocommerce_shop_order_search_fields', array( &$this, 'woocommerce_shop_order_search_fields' ) );
+ * The WC_Seq_Order_Number global object
+ * @name $wc_seq_order_number
+ * @global WC_Seq_Order_Number $GLOBALS['wc_seq_order_number']
+ */
+$GLOBALS['wc_seq_order_number'] = new WC_Seq_Order_Number();
+
+class WC_Seq_Order_Number {
+
+	/** version number */
+	const VERSION = "1.3";
+
+	/** version option name */
+	const VERSION_OPTION_NAME = "woocommerce_seq_order_number_db_version";
+
+	public function __construct() {
+
+		// set the custom order number on the new order.  we hook into wp_insert_post for orders which are created
+		//  from the frontend, and we hook into woocommerce_process_shop_order_meta for admin-created orders
+		add_action( 'wp_insert_post',                      array( $this, 'set_sequential_order_number' ), 10, 2 );
+		add_action( 'woocommerce_process_shop_order_meta', array( $this, 'set_sequential_order_number' ), 10, 2 );
+
+		// return our custom order number for display
+		add_filter( 'woocommerce_order_number',            array( $this, 'get_order_number' ), 10, 2);
+
+		// order tracking page search by order number
+		add_filter( 'woocommerce_shortcode_order_tracking_order_id', array( $this, 'find_order_by_order_number' ) );
+
+		// WC Subscriptions support: prevent unnecessary order meta from polluting parent renewal orders, and set order number for subscription orders
+		add_filter( 'woocommerce_subscriptions_renewal_order_meta_query', array( $this, 'subscriptions_remove_renewal_order_meta' ), 10, 4 );
+		add_action( 'woocommerce_subscriptions_renewal_order_created',    array( $this, 'subscriptions_set_sequential_order_number' ), 10, 4 );
+
+		if ( is_admin() ) {
+			add_filter( 'request',                              array( $this, 'woocommerce_custom_shop_order_orderby' ), 20 );
+			add_filter( 'woocommerce_shop_order_search_fields', array( $this, 'custom_search_fields' ) );
+
+			// sort by underlying _order_number on the Pre-Orders table
+			add_filter( 'wc_pre_orders_edit_pre_orders_request', array( $this, 'custom_orderby' ) );
+			add_filter( 'wc_pre_orders_search_fields',           array( $this, 'custom_search_fields' ) );
+		}
+
+		// Installation
+		if ( is_admin() && ! defined( 'DOING_AJAX' ) ) $this->install();
+	}
+
+
+	/**
+	 * Search for an order with order_number $order_number
+	 *
+	 * @param string $order_number order number to search for
+	 *
+	 * @return int post_id for the order identified by $order_number, or 0
+	 */
+	public function find_order_by_order_number( $order_number ) {
+
+		// search for the order by custom order number
+		$query_args = array(
+					'numberposts' => 1,
+					'meta_key'    => '_order_number',
+					'meta_value'  => $order_number,
+					'post_type'   => 'shop_order',
+					'post_status' => 'publish',
+					'fields'      => 'ids',
+				);
+
+		list( $order_id ) = get_posts( $query_args );
+
+		// order was found
+		if ( $order_id !== null ) return $order_id;
+
+		// if we didn't find the order, then it may be that this plugin was disabled and an order was placed in the interim
+		$order = new WC_Order( $order_number );
+		if ( isset( $order->order_custom_fields['_order_number'][0] ) ) {
+			// _order_number was set, so this is not an old order, it's a new one that just happened to have post_id that matched the searched-for order_number
+			return 0;
+		}
+
+		return $order->id;
+	}
+
+
+	/**
+	 * Set the _order_number field for the newly created order
+	 *
+	 * @param int $post_id post identifier
+	 * @param object $post post object
+	 */
+	public function set_sequential_order_number( $post_id, $post ) {
+		global $wpdb;
+
+		if ( 'shop_order' == $post->post_type && 'auto-draft' != $post->post_status ) {
+			$order_number = get_post_meta( $post_id, '_order_number', true );
+			if ( "" == $order_number ) {
+
+				// attempt the query up to 3 times for a much higher success rate if it fails (due to Deadlock)
+				$success = false;
+				for ( $i = 0; $i < 3 && ! $success; $i++ ) {
+					// this seems to me like the safest way to avoid order number clashes
+					$query = $wpdb->prepare( "
+						INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value)
+						SELECT %d, '_order_number', IF( MAX( CAST( meta_value as UNSIGNED ) ) IS NULL, 1, MAX( CAST( meta_value as UNSIGNED ) ) + 1 )
+							FROM {$wpdb->postmeta}
+							WHERE meta_key='_order_number'",
+						$post_id );
+
+					$success = $wpdb->query( $query );
 				}
-				
-				// Installation
-				if ( is_admin() && ! defined( 'DOING_AJAX' ) ) $this->install();
-			}
-			
-			
-			/**
-			 * Search for an order with order_number $order_number
-			 * 
-			 * @param string $order_number order number to search for
-			 * 
-			 * @return int post_id for the order identified by $order_number, or 0
-			 */
-			public function find_order_by_order_number( $order_number ) {
-				
-				// search for the order by custom order number
-				$query_args = array(
-							'numberposts' => 1,
-							'meta_key'    => '_order_number',
-							'meta_value'  => $order_number,
-							'post_type'   => 'shop_order',
-							'post_status' => 'publish',
-							'fields'      => 'ids'
-						);
-				
-				list( $order_id ) = get_posts( $query_args );
-				
-				// order was found
-				if ( $order_id !== null ) return $order_id;
-				
-				// if we didn't find the order, then it may be that this plugin was disabled and an order was placed in the interim
-				$order = new WC_Order( $order_number );
-				if ( isset( $order->order_custom_fields['_order_number'][0] ) ) {
-					// _order_number was set, so this is not an old order, it's a new one that just happened to have post_id that matched the searched-for order_number
-					return 0;
-				}
-				
-				return $order->id;
-			}
-			
-			
-			/**
-			 * Set the _order_number field for the newly created order
-			 * 
-			 * @param int $post_id post identifier
-			 * @param object $post post object
-			 */
-			public function set_sequential_order_number( $post_id, $post ) {
-				global $wpdb;
-				
-				if ( $post->post_type == 'shop_order' && $post->post_status != 'auto-draft' ) {
-					$order_number = get_post_meta( $post_id, '_order_number', true );
-					if ( $order_number == "" ) {
-						
-						// attempt the query up to 3 times for a much higher success rate if it fails (due to Deadlock)	
-						$success = false;
-						for ( $i = 0; $i < 3 && ! $success; $i++ ) {
-							// this seems to me like the safest way to avoid order number clashes
-							$success = $wpdb->query( 'INSERT INTO ' . $wpdb->postmeta . ' (post_id,meta_key,meta_value) SELECT ' . $post_id . ',"_order_number",if(max(cast(meta_value as UNSIGNED)) is null,1,max(cast(meta_value as UNSIGNED))+1) from ' . $wpdb->postmeta . ' where meta_key="_order_number"' );
-						}
-					}
-				}
-			}
-			
-			
-			/**
-			 * Filter to return our _order_number field rather than the post ID,
-			 * for display.
-			 * 
-			 * @param string $order_number the order id with a leading hash
-			 * @param WC_Order $order the order object
-			 * 
-			 * @return string custom order number, with leading hash
-			 */
-			public function get_order_number( $order_number, $order ) {
-				if ( isset( $order->order_custom_fields['_order_number'] ) ) {
-					return '#' . $order->order_custom_fields['_order_number'][0];
-				}
-				return $order_number;
-			}
-			
-			
-			/** Admin filters ******************************************************/
-			
-			
-			/**
-			 * Admin order table orderby ID operates on our meta _order_number
-			 * 
-			 * @param array $vars associative array of orderby parameteres
-			 * 
-			 * @return array associative array of orderby parameteres
-			 */
-			public function woocommerce_custom_shop_order_orderby( $vars ) {
-				global $typenow, $wp_query;
-			    if ( $typenow != 'shop_order' ) return $vars;
-			    
-			    // Sorting
-				if ( isset( $vars['orderby'] ) ) :
-					if ( 'ID' == $vars['orderby'] ) :
-						$vars = array_merge( $vars, array(
-							'meta_key' 	=> '_order_number',
-							'orderby' 	=> 'meta_value_num'
-						) );
-					endif;
-					
-				endif;
-				
-				return $vars;
-			}
-			
-			
-			/**
-			 * Add our custom _order_number to the set of search fields so that
-			 * the admin search functionality is maintained
-			 * 
-			 * @param array $search_fields array of post meta fields to search by
-			 * 
-			 * @return array of post meta fields to search by
-			 */
-			public function woocommerce_shop_order_search_fields( $search_fields ) {
-				
-				array_push( $search_fields, '_order_number' );
-				
-				return $search_fields;
-			}
-			
-			
-			/** Lifecycle methods ******************************************************/
-			
-			
-			/**
-			 * Run every time.  Used since the activation hook is not executed when updating a plugin
-			 */
-			private function install() {
-				$installed_version = get_option( WC_Seq_Order_Number::VERSION_OPTION_NAME );
-				
-				if ( ! $installed_version ) {
-					// initial install, set the order number for all existing orders to the post id
-					$orders = get_posts( array( 'numberposts' => '', 'post_type' => 'shop_order', 'nopaging' => true ) );
-					if ( is_array( $orders ) ) {
-						foreach( $orders as $order ) {
-							if ( get_post_meta( $order->ID, '_order_number', true ) == '' ) {
-								add_post_meta( $order->ID, '_order_number', $order->ID );
-							}
-						}
-					}
-				}
-				
-				if ( $installed_version != WC_Seq_Order_Number::VERSION ) {
-					$this->upgrade( $installed_version );
-					
-					// new version number
-					update_option( WC_Seq_Order_Number::VERSION_OPTION_NAME, WC_Seq_Order_Number::VERSION );
-				}
-			}
-			
-			
-			/**
-			 * Run when plugin version number changes
-			 */
-			private function upgrade( $installed_version ) {
-				// upgrade code goes here
 			}
 		}
 	}
-	
-	$GLOBALS['wc_seq_order_number'] = new WC_Seq_Order_Number();
+
+
+	/**
+	 * Filter to return our _order_number field rather than the post ID,
+	 * for display.
+	 *
+	 * @param string $order_number the order id with a leading hash
+	 * @param WC_Order $order the order object
+	 *
+	 * @return string custom order number, with leading hash
+	 */
+	public function get_order_number( $order_number, $order ) {
+		if ( isset( $order->order_custom_fields['_order_number'] ) ) {
+			return '#' . $order->order_custom_fields['_order_number'][0];
+		}
+
+		return $order_number;
+	}
+
+
+	/** Admin filters ******************************************************/
+
+
+	/**
+	 * Admin order table orderby ID operates on our meta _order_number
+	 *
+	 * @param array $vars associative array of orderby parameteres
+	 *
+	 * @return array associative array of orderby parameteres
+	 */
+	public function woocommerce_custom_shop_order_orderby( $vars ) {
+		global $typenow, $wp_query;
+		if ( 'shop_order' == $typenow ) return $vars;
+
+		return $this->custom_orderby( $vars );
+	}
+
+
+	/**
+	 * Mofifies the given $args argument to sort on our meta integral _order_number
+	 *
+	 * @since 1.3
+	 * @param array $vars associative array of orderby parameteres
+	 * @return array associative array of orderby parameteres
+	 */
+	public function custom_orderby( $args ) {
+		// Sorting
+		if ( isset( $args['orderby'] ) && 'ID' == $args['orderby'] ) {
+			$args = array_merge( $args, array(
+				'meta_key' => '_order_number',  // sort on numerical portion for better results
+				'orderby'  => 'meta_value_num',
+			) );
+		}
+
+		return $args;
+	}
+
+
+	/**
+	 * Add our custom _order_number to the set of search fields so that
+	 * the admin search functionality is maintained
+	 *
+	 * @param array $search_fields array of post meta fields to search by
+	 *
+	 * @return array of post meta fields to search by
+	 */
+	public function custom_search_fields( $search_fields ) {
+
+		array_push( $search_fields, '_order_number' );
+
+		return $search_fields;
+	}
+
+
+	/** 3rd Party Plugin Support ******************************************************/
+
+
+	/**
+	 * Sets an order number on a subscriptions-created order
+	 *
+	 * @since 1.3
+	 *
+	 * @param WC_Order $renewal_order the new renewal order object
+	 * @param WC_Order $original_order the original order object
+	 * @param int $product_id the product post identifier
+	 * @param string $new_order_role the role the renewal order is taking, one of 'parent' or 'child'
+	 */
+	public function subscriptions_set_sequential_order_number( $renewal_order, $original_order, $product_id, $new_order_role ) {
+		$order_post = get_post( $renewal_order->id );
+		$this->set_sequential_order_number( $order_post->ID, $order_post );
+	}
+
+
+	/**
+	 * Don't copy over order number meta when creating a parent or child renewal order
+	 *
+	 * @since 1.3
+	 *
+	 * @param array $order_meta_query query for pulling the metadata
+	 * @param int $original_order_id Post ID of the order being used to purchased the subscription being renewed
+	 * @param int $renewal_order_id Post ID of the order created for renewing the subscription
+	 * @param string $new_order_role The role the renewal order is taking, one of 'parent' or 'child'
+	 * @return string
+	 */
+	public function subscriptions_remove_renewal_order_meta( $order_meta_query, $original_order_id, $renewal_order_id, $new_order_role ) {
+
+		$order_meta_query .= " AND meta_key NOT IN ( '_order_number' )";
+
+		return $order_meta_query;
+	}
+
+
+	/** Helper Methods ******************************************************/
+
+
+	/**
+	 * Checks if WooCommerce is active
+	 *
+	 * @since  1.3
+	 * @return bool true if WooCommerce is active, false otherwise
+	 */
+	public static function is_woocommerce_active() {
+
+		$active_plugins = (array) get_option( 'active_plugins', array() );
+
+		if ( is_multisite() )
+			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+
+		return in_array( 'woocommerce/woocommerce.php', $active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', $active_plugins );
+	}
+
+
+	/** Lifecycle methods ******************************************************/
+
+
+	/**
+	 * Run every time.  Used since the activation hook is not executed when updating a plugin
+	 */
+	private function install() {
+		$installed_version = get_option( WC_Seq_Order_Number::VERSION_OPTION_NAME );
+
+		if ( ! $installed_version ) {
+			// initial install, set the order number for all existing orders to the post id
+			$orders = get_posts( array( 'numberposts' => '', 'post_type' => 'shop_order', 'nopaging' => true ) );
+			if ( is_array( $orders ) ) {
+				foreach( $orders as $order ) {
+					if ( '' == get_post_meta( $order->ID, '_order_number', true ) ) {
+						add_post_meta( $order->ID, '_order_number', $order->ID );
+					}
+				}
+			}
+		}
+
+		if ( $installed_version != WC_Seq_Order_Number::VERSION ) {
+			$this->upgrade( $installed_version );
+
+			// new version number
+			update_option( WC_Seq_Order_Number::VERSION_OPTION_NAME, WC_Seq_Order_Number::VERSION );
+		}
+	}
+
+
+	/**
+	 * Run when plugin version number changes
+	 */
+	private function upgrade( $installed_version ) {
+		// upgrade code goes here
+	}
 }
