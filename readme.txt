9e7097cdc95890079e82374f9e05a8490f340c6c
Deiva Magalhaes <deiva@skyverge.com>
2020-02-05 11:39:52 -0800

    WooCommerce Sequential Order Numbers: 1.9.3 Versioning

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: SkyVerge, maxrice, tamarazuk, chasewiseman, nekojira, beka.rice
 Tags: woocommerce, order number, sequential order number, woocommerce orders
 Requires at least: 4.4
-Tested up to: 5.2.4
-Stable tag: 1.9.3-dev.1
+Tested up to: 5.3.2
+Stable tag: 1.9.3
 
 This plugin extends WooCommerce by setting sequential order numbers for new orders.
 
@@ -101,7 +101,7 @@ $order_number = $order->get_order_number();
 
 == Changelog ==
 
-= 2020.nn.nn - version 1.9.3-dev.1 =
+= 2020.02.05 - version 1.9.3 =
  * Misc - Add support for WooCommerce 3.9
 
 = 2019.11.05 - version 1.9.2 =
--- woocommerce-sequential-order-numbers.php
+++ woocommerce-sequential-order-numbers.php
@@ -5,7 +5,7 @@
  * Description: Provides sequential order numbers for WooCommerce orders
  * Author: SkyVerge
  * Author URI: http://www.skyverge.com
- * Version: 1.9.3-dev.1
+ * Version: 1.9.3
  * Text Domain: woocommerce-sequential-order-numbers
  * Domain Path: /i18n/languages/
  *
@@ -19,7 +19,7 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  *
  * WC requires at least: 3.0.9
- * WC tested up to: 3.8.0
+ * WC tested up to: 3.9.0
  */
 
 defined( 'ABSPATH' ) or exit;
@@ -33,7 +33,7 @@ class WC_Seq_Order_Number {
 
 
 	/** version number */
-	const VERSION = '1.9.3-dev.1';
+	const VERSION = '1.9.3';
 
 	/** minimum required wc version */
 	const MINIMUM_WC_VERSION = '3.0.9';
