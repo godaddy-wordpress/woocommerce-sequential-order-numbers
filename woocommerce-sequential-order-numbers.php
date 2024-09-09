e1261ff24e6db546066905fa96fc83dfeb859de7
Nabeel Molham <nmolham@godaddy.com>
2024-09-09 15:25:59 +1000

    Bump latest WooCommerce version tested

--- woocommerce-sequential-order-numbers.php
+++ woocommerce-sequential-order-numbers.php
@@ -19,7 +19,7 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  *
  * WC requires at least: 3.9.4
- * WC tested up to: 8.0.3
+ * WC tested up to: 9.2.3
  */
 
 defined( 'ABSPATH' ) or exit;
